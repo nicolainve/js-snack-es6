c65108f52cf6905e92cdf6cf6dd5e2989f7ae9fa
Nicola <nginvernizzi1@gmail.com>
2020-11-03 17:18:21 +0100

    jsnack 2 done

--- js/main.js
+++ js/main.js
@@ -35,3 +35,33 @@ $('.container').html(
         <h3>Il suo peso è di ${weight}kg</h3>
     </div>`
 );
+
+
+/**
+ * JSnack 2
+    Partendo da un array di nomi (scelti da voi) creiamo un nuovo array.
+    Chiedere due numeri all'utente per definire la posizione iniziale e finale dal quale estrarre i valori dall'array originale.
+    Il nuovo array deve contenere gli elementi dell'array originale aventi la posizione (indice array) compresa tra i due numeri inseriti dall'utente.
+*/
+
+const drivers = ['Vettel', 'Raikkonen', 'Hamilton', 'Leclerc', 'Bottas', 'Verstappen'];
+
+let tot = drivers.length - 1;
+
+let min = parseInt( prompt('Inserisci un numero tra 0 e ' + tot).trim() );
+
+let max = parseInt( prompt('Inserisci un numero tra ' + min + ' e ' + tot).trim() );
+
+let newArray = [];
+
+drivers.forEach((element, index) => {
+    if (index <= min && index >= max) {
+        newArray.push(element);
+    }
+});
+
+newArray = drivers.filter((element, index) => {
+    return min <= index && max >= index;
+});
+
+console.log(newArray);
